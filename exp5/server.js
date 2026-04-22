90f277ab374d981b3c13dee84c1b6ca6c0baa1e7
JJacob2006 <johnjacobchd@gmail.com>
2026-04-23 00:17:33 +0530

    added dotenv config

--- exp5/server.js
+++ exp5/server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const mysql = require("mysql2");
 
@@ -8,10 +9,10 @@ app.use(express.json());
 // MYSQL CONNECTION
 // =======================
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "Cuman@1707",
-    database: "middleware_lab"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 db.connect(err => {
